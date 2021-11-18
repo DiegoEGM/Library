ff75c0591e332f5c647dfff91d1cf5f2011c0787
DiegoEGM <39660122+DiegoEGM@users.noreply.github.com>
2021-11-18 09:50:21 -0600

    Add files via upload

---
+++ fft.cpp
@@ -0,0 +1,105 @@
+#ifdef LOCAL
+    #include "debug.h"
+#else
+    #define dbg(...) 0
+#endif
+
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define pb push_back
+#define inf INT_MAX
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int) (x).size())
+using namespace std;
+
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+
+const int N = 2e5 + 3;
+const int mod = 1e9 + 7;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+using db = double; //long double
+const db PI = acos(-1.0);
+using C = complex<db>;
+
+C complex_mult(C &x, C &y) {
+    return C(x.real() * y.real() - x.imag() * y.imag(), x.real() * y.imag() + x.imag() * y.real());
+}
+
+void fft(vector <C> &a, bool inv = false) {
+    int n = a.size();
+    int L = 31 - __builtin_clz(n);
+
+    vector <C> roots(n);
+    for(int i = 0; i < n / 2; i++) {
+        roots[i + n / 2] = polar(db(1.0),  (inv ? -1.0 : 1.0) * 2.0 * PI * db(i) / db(n));
+    }
+    for(int i = n / 2 - 1; i >= 1; i--) {
+        roots[i] = roots[2 * i];
+    }
+
+    vector <int> rev(n);
+    for(int i = 0; i < n; i++) {
+        rev[i] = rev[i / 2] / 2 + ((i & 1) << (L - 1));
+    }
+    for(int i = 0; i < n; i++)
+        if(i < rev[i])
+            swap(a[i], a[rev[i]]);
+
+    for(int len = 1; len < n; len *= 2) {
+        for(int i = 0; i < n; i += 2 * len) {
+            for(int j = 0; j < len; j++) {
+                C z = complex_mult(a[i + j + len], roots[len + j]);
+                a[i + j + len] = a[i + j] - z;
+                a[i + j] += z;
+            }
+        }
+    }
+
+    if(inv)
+        for(int i = 0; i < n; i++)
+            a[i] /= db(n);
+}
+
+vector <C> mult(vector<C> a, vector <C> b) {
+    int N = 1 << (32 - __builtin_clz(sz(a) + sz(b) - 1));
+    while(a.size() < N) a.pb(0);
+    while(b.size() < N) b.pb(0);
+    fft(a);
+    fft(b);
+    vector <C> c(N);
+    for(int i = 0; i < N; i++)
+        c[i] = a[i] * b[i];
+    fft(c, true);
+    return c;
+}
+
+vector <int> mult(vector<int> &A, vector <int> &B) {
+    int n = A.size(), m = B.size();
+    vector <C> a(n), b(m), c;
+    for(int i = 0; i < n; i++)
+        a[i] = A[i];
+    for(int i = 0; i < m; i++)
+        b[i] = B[i];
+    c = mult(a, b);
+    vector <int> C(c.size());
+    for(int i = 0; i < c.size(); i++) {
+        C[i] = int(round(real(c[i])));
+    }
+    while(C.size() > 1 && C.back() == 0)
+        C.pop_back();
+    return C;
+}
+
+int main() {
+    vector<int> A = {4, 1, 5}, B = {3, 1};
+    vector <int> C = mult(A, B);
+    for(auto x : C) {
+        printf("%d ", x);
+    }
+    return 0;
+}

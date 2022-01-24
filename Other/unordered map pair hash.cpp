038d77586fa0530f0ddd81126e815391dfcf874f
DiegoEGM <39660122+DiegoEGM@users.noreply.github.com>
2022-01-24 17:04:15 -0600

    Create unordered map pair hash.cpp

---
+++ Other/unordered map pair hash.cpp
@@ -0,0 +1,5 @@
+struct HASH{
+size_t operator()(const pair<int,int>&x)const{
+    return hash<long long>()(((long long)x.first)^(((long long)x.second)<<32));
+}};
+unordered_map <pair<int, int>, int, HASH> M;

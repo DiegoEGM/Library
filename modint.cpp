f0beaf587cb6fc4db105acb8726816eccf0d3f43
DiegoEGM <39660122+DiegoEGM@users.noreply.github.com>
2022-01-24 08:43:36 -0600

    Update modint.cpp

--- modint.cpp
+++ modint.cpp
@@ -56,6 +56,7 @@ void init_fact(int n) {
     fact[0] = mint(1);
     for(int i = 1; i <= n; i++)
         fact[i] = fact[i - 1] * mint(i);
+
     invfact[n] = fact[n].inv();
     for(int i = n - 1; i >= 0; i--)
         invfact[i] = mint(i + 1) * invfact[i + 1];
@@ -66,8 +67,31 @@ mint nck(int n, int k) {
     return fact[n] * invfact[n - k] * invfact[k];
 }
 
-int main() {
 
+mint interpolate(vector<mint> &sample, mint x) {
+    int n = sz(sample);
+    init_fact(n); //if its not generated before
+    mint fac = fact[n - 1];
+    vector<mint> pref(n), suf(n);
+    pref[0] = mint(1);
+    for(int i = 2; i <= n; i++) {
+        pref[i - 1] = pref[i - 2] * (x - (i - 1));
+    }
+    suf[n - 1] = mint(1);
+    for(int i = n - 2; i >= 0; i--)
+        suf[i] = suf[i + 1] * (x - (i + 2));
+    mint ans = 0;
+    for(int i = 1; i <= n; i++) {
+        mint term = invfact[i - 1] * invfact[n - i];
+        if((n - i) % 2 == 1)
+            term = term * mint(-1);
+        term *= pref[i - 1] * suf[i - 1] * sample[i - 1];
+        ans += term;
+    }
+    return ans;
+}
+
+int main() {
     return 0;
 }
 

b6d7ba1a6f69f6b65ecaf18436bb6f2175ad1547
DiegoEGM <39660122+DiegoEGM@users.noreply.github.com>
2022-01-24 17:05:09 -0600

    Update unordered map pair hash.cpp

--- Other/unordered map pair hash.cpp
+++ Other/unordered map pair hash.cpp
@@ -1,6 +1,6 @@
+//very useful to speed up string hashing with two bases!
 struct HASH{
 size_t operator()(const pair<int,int>&x)const{
     return hash<long long>()(((long long)x.first)^(((long long)x.second)<<32));
 }};
 unordered_map <pair<int, int>, int, HASH> M;
-//very useful to speed up string hashing with two bases!

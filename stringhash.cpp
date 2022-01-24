5f1f15042dc8a9a4f20b2f6ea334bd3be01e65cb
DiegoEGM <39660122+DiegoEGM@users.noreply.github.com>
2022-01-24 17:01:16 -0600

    Update stringhash.cpp

--- stringhash.cpp
+++ stringhash.cpp
@@ -11,11 +11,8 @@ using ll = long long;
 using pii = pair<int, int>;
 using vi = vector<int>;
 
-/*
-double hashing with fixed mods
-and random bases.
-maybe do something about the mults (taking mod only if necessary?)
-*/
+//double hashing with fixed mods and random bases
+
 const int N = 1e5+3;
 
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
@@ -25,11 +22,11 @@ int B[2];
 const int M[2] = {1000000007, 998244353};
 int pot[2][N];
 
-struct string_hash {
+struct StringHash {
     vi ht[2];
     int n_;
 
-    string_hash(char *s, int nn_) { //string s, s.size()
+    StringHash(char *s, int nn_) { //string s, s.size()
         ht[0].resize(nn_); ht[1].resize(nn_); n_ = nn_;
 
         for(int i = 0; i < 2; i++) {
@@ -41,7 +38,10 @@ struct string_hash {
         }
     }
     array <int, 2> range_hash(int l, int r) {
-        if(l > r || l < 0 || r >= n_) { printf("BAD RANGE\n"); return array<int,2>{-1,-1}; }
+        if(l > r || l < 0 || r >= n_) {
+            printf("tried to access %d %d\n", l, r);
+            assert(false);
+        }
         array <int, 2> ret = {ht[0][r], ht[1][r]};
         if(l) for(int i = 0; i < 2; i++) {
             ret[i] -= int((1ll * ht[i][l - 1] * 1ll * pot[i][r - l + 1]) % M[i]);
@@ -52,8 +52,8 @@ struct string_hash {
 
 };
 
-//first param in rnd depends on alphabet size. maybe move [a..z] to [1..z] (remember a = 0 is bad)
 void get_powers(int n_) {
+    //bases should be biffer than coefs
     B[0] = rnd('z' + 1, M[0] - 1);
     B[1] = rnd('z' + 1, M[1] - 1);
     for(int i = 0; i < 2; i++) {
@@ -75,7 +75,7 @@ int main() {
     m = strlen(t);
     get_powers(max(n, m));
 
-    string_hash H1(s, n), H2(t, m);
+    StringHash H1(s, n), H2(t, m);
 
     if(H1.range_hash(0, n - 1) == H2.range_hash(0, m - 1))
         printf("lul\n");

ece690b72a07dad59231684deaf26fe36f62d26f
DiegoEGM <39660122+DiegoEGM@users.noreply.github.com>
2022-01-25 16:54:48 -0600

    Update suffixarray.cpp

--- suffixarray.cpp
+++ suffixarray.cpp
@@ -12,39 +12,6 @@ using pii = pair<int, int>;
 using vi = vector<int>;
 using ld = long double;
 
-//if memory limit low change to segment tree
-template <typename T>
-struct SparseTable {
-    int n, L;
-    vector <T> lg;
-    T ID;
-    vector <vector<T>> table;
-
-    T comb(T x, T y) {
-        return min(x, y);
-    }
-
-    SparseTable(vector <T> a, T ID_) {
-        n = a.size();
-        lg.resize(n + 1);
-        ID = ID_;
-        for(int i = 2; i <= n; i++)
-            lg[i] = lg[i >> 1] + 1;
-        L = lg[n] + 1;
-        table = vector<vector<T>> (n, vector<T>(L));
-        for(int i = 0; i < n; i++)
-            table[i][0] = a[i];
-        for(int k = 1; k < L; k++)
-            for(int i = 0; i + (1 << k) - 1 < n; i++)
-                table[i][k] = comb(table[i][k - 1], table[i + (1 << (k - 1))][k - 1]);
-    }
-
-    T query(int l, int r) {
-        int k = lg[r - l + 1];
-        return comb(table[l][k], table[r - (1 << k) + 1][k]);
-    }
-};
-
 template <class T = string>
 struct SuffixArray {
     int n;
@@ -52,7 +19,7 @@ struct SuffixArray {
     vi lcp;
 
     SuffixArray(T s) {
-        s.pb(*min_element(all(s)) - 1);
+        s.pb(*min_element(all(s)) - 1); //smaller than everything in s
         n = sz(s);
         p.resize(n), c.resize(n);
         vi new_c(n), new_p(n), ptr(n);
@@ -99,16 +66,14 @@ struct SuffixArray {
         }
     }
 
-    template <class RMQ>
+    template <class RMQ> //declare some SparseTable<int,Minimizer<int>> st, then call this as .LCP(i, j, st)
     int LCP(int i, int j, RMQ &rmq) {
         if(i == j) return n - i - 1;
         int l = c[i];
         int r = c[j];
         if(l > r) swap(l, r);
         return rmq.query(l, r - 1);
-
     }
-
 };
 
 int main() {

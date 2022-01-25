76a355c0ee50f55172f1db1f58b36564848fc3a4
DiegoEGM <39660122+DiegoEGM@users.noreply.github.com>
2022-01-25 16:52:57 -0600

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define pb push_back
+#define inf INT_MAX
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int) (x).size())
+using namespace std;
+
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using ld = long double;
+
+const int N = 2e5 + 3;
+const int mod = 1e9 + 7;
+
+void solve() {
+  
+}
+
+int main() {
+    int t = 1;
+    scanf("%d", &t);
+    while(t--) {
+        solve();
+    }
+    return 0;
+
+}

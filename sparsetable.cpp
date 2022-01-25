1e39573d253cc81db404ddbf9717355c476d3803
DiegoEGM <39660122+DiegoEGM@users.noreply.github.com>
2022-01-25 16:43:00 -0600

    Update sparsetable.cpp

--- sparsetable.cpp
+++ sparsetable.cpp
@@ -14,21 +14,16 @@ using vi = vector<int>;
 const int N = 2e5 + 3;
 const int mod = 1e9 + 7;
 
-template <typename T>
+template <class T, class C>
 struct SparseTable {
     int n, L;
+    C combine;
     vector <T> lg;
-    T ID;
     vector <vector<T>> table;
 
-    T comb(T x, T y) {
-        return x + y;
-    }
-
-    SparseTable(vector <T> a, T ID_) {
+    SparseTable(vector <T> a) {
         n = a.size();
         lg.resize(n + 1);
-        ID = ID_;
         for(int i = 2; i <= n; i++)
             lg[i] = lg[i >> 1] + 1;
         L = lg[n] + 1;
@@ -37,30 +32,24 @@ struct SparseTable {
             table[i][0] = a[i];
         for(int k = 1; k < L; k++)
             for(int i = 0; i + (1 << k) - 1 < n; i++)
-                table[i][k] = comb(table[i][k - 1], table[i + (1 << (k - 1))][k - 1]);
+                table[i][k] = combine(table[i][k - 1], table[i + (1 << (k - 1))][k - 1]);
     }
 
     T query(int l, int r) {
         int k = lg[r - l + 1];
-        return comb(table[l][k], table[r - (1 << k) + 1][k]);
+        return combine(table[l][k], table[r - (1 << k) + 1][k]);
     }
+};
 
-    T query2(int l, int r) { //not idempodent
-        int k = r - l + 1;
-        T ret = ID;
-        for(int i = 0; i < L; i++)
-            if(k & (1 << i)) {
-                ret = comb(ret, table[l][i]);
-                l += (1 << i);
-            }
-        assert(l == r + 1);
-        return ret;
-    }
-
+template<typename T>
+struct Minimizer {
+    T operator()(T x, T y) const { return min(x, y); }
 };
 
 int main() {
- 
-    
+    vector <int> lol = {1, 2, 3};
+    SparseTable <int, Minimizer<int>> st(lol);
+
     return 0;
 }
+

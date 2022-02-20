b33bbdb7b5dea9ef81105044883bb18503509ccf
DiegoEGM <39660122+DiegoEGM@users.noreply.github.com>
2022-02-19 18:43:37 -0600

    Update kmp.cpp

--- kmp.cpp
+++ kmp.cpp
@@ -1,12 +1,7 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-/*
-O(|s|).
-note that f[i + 1] <= f[i] + 1.
-f[i] is the length of the maximum proper preffix of s[0..i]
-that is also a suffix of s[0..i]
-*/
+//O(|s|).
+//note that f[i + 1] <= f[i] + 1.
+//f[i] is the length of the maximum proper preffix of s[0..i]
+//that is also a suffix of s[0..i].
 vector <int> kmp(string s) {
     int n = s.size();
     vector <int> f(n);
@@ -21,8 +16,7 @@ vector <int> kmp(string s) {
     return f;
 }
 
-/* returns the positions of t where s is located.
-*/
+//returns the positions of t where s is located.
 vector <int> get_ocurrences(string s, string t) {
     vector <int> f = kmp(s + "#" + t); //'#' must not be in s or t.
     vector <int> oc;
@@ -34,15 +28,5 @@ vector <int> get_ocurrences(string s, string t) {
     return oc;
 }
 
-int main() {
+//see https://cp-algorithms.com/string/prefix-function.html for cool applications
 
-    return 0;
-}
-
-/*
-see https://cp-algorithms.com/string/prefix-function.html
-extra stuff: counting # dif substrings in O(n^2),
-counting how many times each preffix appears.
-worth nothing that the get ocurrences and the dif substrings
-could be also calculated with hashing, but this one is deterministic.
-*/

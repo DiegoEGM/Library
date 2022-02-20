05bc2cfc5f23a7167b6ea765f8c36b60c6f991e8
DiegoEGM <39660122+DiegoEGM@users.noreply.github.com>
2022-02-19 18:18:24 -0600

    Update modint and combi.cpp

--- modint and combi.cpp
+++ modint and combi.cpp
@@ -1,18 +1,3 @@
-#include <bits/stdc++.h>
-#define pb push_back
-#define inf INT_MAX
-#define all(x) (x).begin(), (x).end()
-#define sz(x) ((int) (x).size())
-using namespace std;
-
-using ll = long long;
-using pii = pair<int, int>;
-using pl = pair<ll, ll>;
-using vi = vector<int>;
-
-const int N = 2e5 + 3;
-const int mod = 1e9 + 7;
-
 template <class T>
 T bexp(T a, ll p) {
     T x = T(1), y = a;
@@ -28,18 +13,18 @@ T bexp(T a, ll p) {
 template <int MOD = mod>
 struct ModInt {
     int val;
-    ModInt(int v = 0) {
+
+    ModInt(ll v = 0) {
+        v %= MOD;
+        if(v < 0) v += MOD;
         val = v;
-        if(val >= mod) val -= mod;
-        if(val < 0) val += mod;
-       // assert(val >= 0 && val < mod);
     }
 
-    ModInt inv() { return bexp<ModInt>(*this, mod - 2); }
+    ModInt inv() { return bexp<ModInt>(*this, MOD - 2); }
 
     ModInt operator + (ModInt o) { return ModInt(val + o.val); }
     ModInt operator - (ModInt o) { return ModInt(val - o.val); }
-    ModInt operator * (ModInt o) { return ModInt(int((1ll * val * 1ll * o.val) % mod)); }
+    ModInt operator * (ModInt o) { return ModInt(int((1ll * val * 1ll * o.val) % MOD)); }
     ModInt operator / (ModInt o) { return *this * o.inv(); }
 
     ModInt& operator += (ModInt o) { return *this = *this + o; }
@@ -66,11 +51,10 @@ mint nck(int n, int k) {
     if(k > n || n < 0) return mint(0);
     return fact[n] * invfact[n - k] * invfact[k];
 }
-
-
+//returns f(x) assuming sample = {f(1)...f(n)} and f is <= n - 1 degree polynomial
 mint interpolate(vector<mint> &sample, mint x) {
     int n = sz(sample);
-    init_fact(n); //if its not generated before
+    init_fact(n); //comment out if previously generated
     mint fac = fact[n - 1];
     vector<mint> pref(n), suf(n);
     pref[0] = mint(1);
@@ -91,7 +75,4 @@ mint interpolate(vector<mint> &sample, mint x) {
     return ans;
 }
 
-int main() {
-    return 0;
-}
 

d18b0cff40fbefdb38e0e9576a00d5f50d11f7e7
DiegoEGM <39660122+DiegoEGM@users.noreply.github.com>
2022-02-19 18:13:21 -0600

    Update fenwick2d.cpp

--- fenwick2d.cpp
+++ fenwick2d.cpp
@@ -1,14 +1,10 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-
 template <class T>
 struct Fenwick2D {
     vector <vector<T>> tree;
-    int n_, m_;
-    Fenwick2D(int n, int m) : n_(n), m_(m), tree(n + 1, vector<T>(m + 1)) {}
+    int n, m;
+    Fenwick2D(int n_, int m_) : n(n_), m(m_), tree(n + 1, vector<T>(m + 1)) {}
     T get(int i, int j) {
-        if(i <= 0 || i > n_ || j <= 0 || j > m_) return T(0);
+        if(i <= 0 || i > n || j <= 0 || j > m) return T(0);
         T sum = T(0);
         for(; i; i -= (i & -i))
             for(int k = j; k; k -= (k & -k))
@@ -16,15 +12,11 @@ struct Fenwick2D {
         return sum;
     }
     void upd(int i, int j, T val) {
-        for(; i <= n_; i += (i & -i))
-            for(int k = j; k <= m_; k += (k & -k))
+        for(; i <= n; i += (i & -i))
+            for(int k = j; k <= m; k += (k & -k))
                 tree[i][k] += val;
     }
     T query(int a, int b, int c, int d) { //(a, b) is upper left, (c, d) is lower right
         return get(c, d) + get(a - 1, b - 1) - get(c, b - 1) - get(a - 1, d);
     }
 };
-
-int main() {
-    return 0;
-}

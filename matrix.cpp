adf4bac9fd746c30f0ec52bebd84ad048b78f05a
DiegoEGM <39660122+DiegoEGM@users.noreply.github.com>
2022-02-20 20:51:24 -0600

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -1,50 +1,23 @@
-#include <bits/stdc++.h>
-#define ft first
-#define sd second
-#define pb push_back
-using namespace std;
-
-using ll = long long;
-
-const int MX = 101; //idk
-const int MOD = 1e9 + 7;
-
-int add(int x, int y) {
-    return x + y >= MOD ? x + y - MOD : x + y;
-}
-
-int mul(int x, int y) {
-    ll prod = (ll)x * (ll)y;
-    return prod >= MOD ? prod % MOD : prod;
-}
-
-int dec(int x, int y) {
-    return x < y ? MOD - y + x : x - y;
-}
-
+//most often T = mint
+//(see modint.cpp)
+template <class T>
 struct matrix {
-    int m[MX][MX];
+    vector<vector<T>> m;
     int r, c;
 
-    matrix() {
-        memset(m, 0, sizeof m);
-        r = c = 0;
-    }
-    matrix(int r_, int c_) {
+    matrix(int r_, int c_) : r(r_), c(c_) {
         r = r_; c = c_;
-        memset(m, 0, sizeof m);
+        m = vector<vector<T>> (r, vector<T>(c, T(0)));
     }
 
     matrix operator * (const matrix &b) {
-        //N = #filas de A = #columnas e B
         assert(c == b.r);
-        matrix prd = matrix(r, b.c);
+        matrix prod = matrix(r, b.c);
         for(int i = 0; i < r; i++)
             for(int j = 0; j < b.c; j++)
-                for(int k = 0; k < c; k++) {
-                    prd.m[i][j] = add(prd.m[i][j] , mul(m[i][k], b.m[k][j]));
-                }
-        return prd;
+                for(int k = 0; k < c; k++)
+                    prod.m[i][j] = prod.m[i][j] + m[i][k] * b.m[k][j];
+        return prod;
     }
 
      matrix operator + (const matrix &b) {
@@ -52,7 +25,7 @@ struct matrix {
         matrix res(r, c);
         for(int i = 0; i < r; i++)
             for(int j = 0; j < c; j++)
-                res.m[i][j] = add(m[i][j], b.m[i][j]);
+                res.m[i][j] = m[i][j] + b.m[i][j];
         return res;
     }
 
@@ -61,33 +34,27 @@ struct matrix {
         matrix res(r, c);
         for(int i = 0; i < r; i++)
             for(int j = 0; j < c; j++)
-                res.m[i][j] = dec(m[i][j], b.m[i][j]);
+                res.m[i][j] = m[i][j] - b.m[i][j];
         return res;
     }
 };
 
-matrix binexp(matrix a, int p) {
+template <class T>
+matrix <T> identity(int n) {
+    matrix <T> id(n, n);
+    for(int i = 0; i < n; i++) id.m[i][i] = T(1);
+    return id;
+}
+
+template <class T>
+matrix <T> binexp(matrix <T> a, ll p) {
     assert(a.r == a.c && p >= 0);
-
-    matrix ret = matrix(a.r, a.c);
-    for(int i = 0; i < a.r; i++)
-        ret.m[i][i] = 1;
-
+    matrix ret = identity <T> (a.r);
     while(p) {
         if(p % 2 == 1)
-            ret = (ret * a);
-        a = (a * a);
+            ret = ret * a;
+        a = a * a;
         p /= 2;
     }
     return ret;
 }
-
-int main() {
-    matrix M(2, 2);
-    M.m[0][1] = M.m[1][0] = M.m[1][1] = 1;
-    M = binexp(M, 9);
-    printf("%d\n", M.m[1][0] + M.m[1][1]); //89: above is fibonacci
-}
-//https://github.com/bqi343/USACO/blob/master/Implementations/content/numerical/Matrix%20(11.3)/Matrix.h <- benq stuff
-//https://codeforces.com/blog/entry/67776 cf tuto
-//https://codeforces.com/blog/entry/21189 another cf tuto

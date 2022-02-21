5c4e601fee87b1f366b71c98052a86eb88629669
DiegoEGM <39660122+DiegoEGM@users.noreply.github.com>
2022-02-21 07:34:41 -0600

    Update sparsetable.cpp

--- sparsetable.cpp
+++ sparsetable.cpp
@@ -1,22 +1,6 @@
-#include <bits/stdc++.h>
-#define f first
-#define s second
-#define pb push_back
-#define inf INT_MAX
-#define all(x) (x).begin(), (x).end()
-#define sz(x) ((int) (x).size())
-using namespace std;
-
-using ll = long long;
-using pii = pair<int, int>;
-using vi = vector<int>;
-
-const int N = 2e5 + 3;
-const int mod = 1e9 + 7;
-
 template <class T, class C>
 struct SparseTable {
-    int n, L;
+    int n, l;
     C combine;
     vector <T> lg;
     vector <vector<T>> table;
@@ -26,11 +10,11 @@ struct SparseTable {
         lg.resize(n + 1);
         for(int i = 2; i <= n; i++)
             lg[i] = lg[i >> 1] + 1;
-        L = lg[n] + 1;
-        table = vector<vector<T>> (n, vector<T>(L));
+        l = lg[n] + 1;
+        table = vector<vector<T>> (n, vector<T>(l));
         for(int i = 0; i < n; i++)
             table[i][0] = a[i];
-        for(int k = 1; k < L; k++)
+        for(int k = 1; k < l; k++)
             for(int i = 0; i + (1 << k) - 1 < n; i++)
                 table[i][k] = combine(table[i][k - 1], table[i + (1 << (k - 1))][k - 1]);
     }
@@ -41,15 +25,10 @@ struct SparseTable {
     }
 };
 
-template<typename T>
+template<class T>
 struct Minimizer {
     T operator()(T x, T y) const { return min(x, y); }
 };
 
-int main() {
-    vector <int> lol = {1, 2, 3};
-    SparseTable <int, Minimizer<int>> st(lol);
-
-    return 0;
-}
-
+template <class T>
+using RMQ = SparseTable<T, Minimizer<T>>;

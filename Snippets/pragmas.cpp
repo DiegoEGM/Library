c98c78126d02c9c9bd46a3a00c91140125470630
DiegoEGM <39660122+DiegoEGM@users.noreply.github.com>
2022-05-09 08:43:45 -0500

    Update pragmas.cpp

--- Snippets/pragmas.cpp
+++ Snippets/pragmas.cpp
@@ -1,5 +1,5 @@
 #pragma GCC optimize ("O3")
 #pragma GCC target ("sse4")
-##que mas?? xd
 #pragma GCC optimize("Ofast,unroll-loops")
 #pragma GCC target("avx2,bmi,bmi2,popcnt,lzcnt")
+//disclaimer: i dont know when to use which :p

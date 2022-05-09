060fc635dcb211db43b9d76e6fd119bd2b915e80
DiegoEGM <39660122+DiegoEGM@users.noreply.github.com>
2022-05-09 08:41:44 -0500

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -2,7 +2,6 @@
 #define f first
 #define s second
 #define pb push_back
-#define inf INT_MAX
 #define all(x) (x).begin(), (x).end()
 #define sz(x) ((int) (x).size())
 using namespace std;
@@ -12,19 +11,20 @@ using pii = pair<int, int>;
 using vi = vector<int>;
 using ld = long double;
 
-const int N = 2e5 + 3;
+const int inf = INT_MAX;
+const ll linf = LLONG_MAX;
 const int mod = 1e9 + 7;
+const int N = 2e5 + 3;
 
 void solve() {
-  
+
 }
 
 int main() {
     int t = 1;
     scanf("%d", &t);
     while(t--) {
-        solve();
+         solve();
     }
     return 0;
-
 }

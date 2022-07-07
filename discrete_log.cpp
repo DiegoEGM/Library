fb3cf586620705fb8258b22df7a0032e64cbb348
DiegoEGM <39660122+DiegoEGM@users.noreply.github.com>
2022-07-07 11:57:07 -0500

    Add files via upload

---
+++ discrete_log.cpp
@@ -0,0 +1,39 @@
+//returns minimum x such that a^x = b mod m, assuming 0^0 = 1.
+//O(sqrt(m)) + O(removing the gcds).
+//tested: https://judge.yosupo.jp/submission/94862
+ll discrete_log(ll a, ll b, ll m) {
+    a %= m; b %= m;
+    if(b == 1) return 0;
+    if(a == 0) {
+        if(b == 0) return (m != 1);
+        if(b == 1) return 0;
+        return -1;
+    }
+    int shift = 0;
+    ll g, coef = 1;
+    while((g = gcd(a, m)) > 1) { //converting to coprime case
+        if(b == coef) return shift;
+        if(b % g != 0) return -1;
+        b /= g;
+        m /= g;
+        (coef *= a / g) %= m;
+        shift++;
+    }
+    ll SQ = sqrtl(m) + 1;
+    vector <pair<ll, int>> small = {{b, 0}};
+    ll a_pw = 1ll;
+    for(int i = 1; i <= SQ; i++) {
+        (a_pw *= a) %= m;
+        small.pb({(a_pw * b) % m, -i});
+    }
+    sort(all(small));
+    ll cur = coef;
+    for(int i = 1; i <= SQ; i++) {
+        (cur *= a_pw) %= m;
+        int j = lower_bound(all(small), pair<ll, int>{cur, -inf}) - small.begin();
+        if(j <= SQ && small[j].f == cur) {
+            return (SQ * 1ll * i + small[j].s) + shift;
+        }
+    }
+    return -1;
+}

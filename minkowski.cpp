ca913fe3d9fb87a85da95ff0a49b0de0b6db5456
DiegoEGM <39660122+DiegoEGM@users.noreply.github.com>
2022-07-27 13:21:04 -0500

    Add files via upload

---
+++ minkowski.cpp
@@ -0,0 +1,27 @@
+//Tested on: https://codeforces.com/contest/87/problem/E
+//needs point.cpp
+
+//Returns the vertices of the Minkowski sum of P and Q.
+//P and Q must previously be ordered by (x, y). (leftmost lowest first).
+//Sum is returned with the same order. Complexity: O(|P| + |Q|).
+vector <pt> Minkowski(vector <pt> P, vector <pt> Q) {
+    P.pb(P[0]);
+    Q.pb(Q[0]);
+    vector <pt> ret = {P[0] + Q[0]};
+    int i = 0, j = 0;
+    while(i < sz(P) - 1 || j < sz(Q) - 1) {
+        pt nex;
+        if(i == sz(P) - 1 || (j < sz(Q) - 1 && (P[i + 1] - P[i]).cross(Q[j + 1] - Q[j]) > 0)) {
+            nex = P[i] + Q[++j];
+        }
+        else {
+            nex = P[++i] + Q[j];
+        }
+        while(sz(ret) >= 2 && (nex - ret[sz(ret) - 2]).cross(ret[sz(ret) - 1] - ret[sz(ret) - 2]) == 0) {
+            ret.pop_back();
+        }
+        ret.pb(nex);
+    }
+    ret.pop_back();
+    return ret;
+}

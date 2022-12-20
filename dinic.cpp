69a02a135d8f7e71f745d0507378069062ecc717
DiegoEGM <39660122+DiegoEGM@users.noreply.github.com>
2022-12-20 16:56:24 -0600

    Update dinic.cpp
    removed comments, added scaling

--- dinic.cpp
+++ dinic.cpp
@@ -1,25 +1,25 @@
 template <class T = int>
 struct Dinic {
-    //u -> v with capacity cap.
-    //u is not used in algorithm, but its useful for restoring cuts etc.
     struct Edge {
         int u, v;
         T cap;
         Edge(int u_, int v_, T c_) : u(u_), v(v_), cap(c_) {}
     };
-    int n, s, t; //s = source, t = sink.
-    vi pt, dist; //first edge to consider. distance in level graph.
-    vector <Edge> edges; //even indices are original edges, odd are their reverse.
-    vector <vi> adj; //adjacency is kept as indices of edges in 'edges' array.
-    Dinic(int n_, int s_, int t_)  : n(n_), s(s_), t(t_),
-                        pt(n + 1), dist(n + 1), adj(n + 1) {}
+    bool scaling;
+    int n, s, t;
+    T lim;
+    vi pt, dist;
+    vector <Edge> edges;
+    vector <vi> adj;
+    Dinic(int n_, int s_, int t_, bool sc = false)  : n(n_), s(s_), t(t_),
+                        pt(n + 1), dist(n + 1), adj(n + 1), scaling(sc) {}
     void add_edge(int u, int v, T cap) {
         edges.pb(Edge(u, v, cap));
         edges.pb(Edge(v, u, T(0)));
         adj[u].pb(sz(edges) - 2);
         adj[v].pb(sz(edges) - 1);
     }
-    bool bfs() { //preparing the level graph
+    bool bfs() {
         fill(all(dist), n);
         queue <int> q;
         dist[s] = 0;
@@ -29,6 +29,7 @@ struct Dinic {
             q.pop();
             for(auto e : adj[u]) {
                 if(edges[e].cap == T(0)) continue;
+                if(scaling && edges[e].cap < lim) continue;
                 int v = edges[e].v;
                 if(dist[v] == n) {
                     dist[v] = dist[u] + 1;
@@ -46,8 +47,6 @@ struct Dinic {
             T c = edges[i].cap;
             if(!c || dist[v] != dist[u] + 1) continue;
             T can_push = dfs(v, min(flow, c));
-            //dont explicity mantain the amount of flow pushed; pushing is equiv.
-            //to decreasing capacity, but increasing it in the reversed edge.
             if(can_push > 0) {
                 edges[i].cap -= can_push;
                 edges[i ^ 1].cap += can_push;
@@ -58,15 +57,17 @@ struct Dinic {
     }
     T max_flow() {
         T ans(0);
-        while(bfs()) { //(while augmenting path exists)
-            fill(all(pt), 0); //need to consider all edges again
-            while(true) { //(while i can still find a path for the 'blocking flow')
-                T flow = dfs(s, T(1e9)); //infinity
-                if(flow > 0) ans += flow;
-                else break;
+        for(lim = scaling ? (1 << 30) : 1; lim; lim /= 2) {
+            while(bfs()) {
+                fill(all(pt), 0);
+                while(true) {
+                    T flow = dfs(s, T(1e9));
+                    if(flow > 0) ans += flow;
+                    else break;
+                }
             }
+
         }
         return ans;
     }
 };
-

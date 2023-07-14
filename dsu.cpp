9e245ddd6083659d73a101d465aadc3d1e615318
DiegoEGM <39660122+DiegoEGM@users.noreply.github.com>
2023-07-14 11:09:12 -0600

    Add files via upload

---
+++ dsu.cpp
@@ -0,0 +1,16 @@
+struct dsu {
+    int n;
+    vi info;
+    dsu(int n) : n(n), info(n + 1, -1) {}
+    int tag(int u) {
+        return info[u] < 0 ? u : info[u] = tag(info[u]);
+    }
+    bool join(int u, int v) {
+        u = tag(u), v = tag(v);
+        if(u == v) return false;
+        if(info[u] > info[v]) swap(u, v);
+        info[v] += info[u];
+        info[u] = v;
+        return true;
+    }
+};
